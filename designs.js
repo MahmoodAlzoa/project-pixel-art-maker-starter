c41ad3cfd0038b3289ba53f60ab02691bcb35e3a
MahmoodAlzoa <56774683+MahmoodAlzoa@users.noreply.github.com>
2019-11-11 00:31:07 +0000

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+// color input
+const colorPicker = document.getElementById('colorPicker');
+// size input
+const sizePicker = document.getElementById('sizePicker');
+// When the user write the Height and width and press the Sumbit button, this call the makeGrid function 
+document.querySelector('input[type="submit"]').addEventListener('click', Submit);
+function Submit(event){
+  event.preventDefault();
+  makeGrid();
 }
+function makeGrid() {
+  // Define variables 
+  const canvas = document.getElementById('pixelCanvas');
+  var Height = document.getElementById('inputHeight').value;
+  var Width = document.getElementById('inputWidth').value;
+  grid.innerHTML=null;
+
+  // Grid Process 
+  for (var v = 0; v < Height; v++){
+    var row = document.createElement('tr')
+    canvas.appendChild(row);
+    for(var h = 0; h < Width; h++){
+      var colum = document.createElement('td')
+      // ColorPicker Process
+      colum.addEventListener("click",function(eve) {
+        var color = colorPicker.value;
+        eve.target.style.backgroundColor = color;
+      });
+      canvas.lastChild.appendChild(colum);
+    }
+  }
+} 
